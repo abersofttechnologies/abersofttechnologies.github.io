37ff87233f9767140712237ba6cf1d76a5f89f1a
Cong Fandi <cong.fandi@abersoft.se>
2021-04-15 16:10:35 +0700

    updating tawkto location

--- src/components/Footer/TheFooter.vue
+++ src/components/Footer/TheFooter.vue
@@ -77,3 +77,17 @@
     }
 
 </style>
+
+<div id='tawk_6077f25ef7ce1827093aa012'></div>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_6077f25ef7ce1827093aa012';
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6077f25ef7ce1827093aa012/1f3a8rohd';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);})();
+</script>
+<!--End of Tawk.to Script-->
